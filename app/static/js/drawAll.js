c4dc523e7615f6cd7130cb5d2ad2d4cf31f04a59
erohinaelena <erohinaelena1@yandex.ru>
2016-11-03 17:09:57 +0300

    update graph, return working map click

--- app/static/js/drawAll.js
+++ app/static/js/drawAll.js
@@ -55,7 +55,7 @@ d3.xml("static/data/testdata.xml", function (xmlData) {
             var nodeX = d.coords.x - x;
             var nodeY = d.coords.y - y;
             var currentDist = Math.sqrt(nodeX * nodeX + nodeY * nodeY);
-            var currentFloor = (d.id*1 <= 111) ? 4 : (d.id.toString()[0] == "5")? 5 : 2;
+            var currentFloor = (d.id * 1 <= 111) ? 4 : (d.id.toString()[0] == "5") ? 5 : 2;
             if (currentDist < distance && currentFloor == global_floor) {
                 distance = currentDist;
                 node = d
@@ -101,45 +101,54 @@ function updateGroup(data, xmlData) {
     if (!weekDay) {
 
     }
-    Object.keys(groupLabels).forEach(function(group) {
-        data[weekDay][group].forEach(function(item){
+    Object.keys(groupLabels).forEach(function (group) {
+        var coords = 0;
+        data[weekDay][group].forEach(function (item) {
             var start = item.Time_start.split(":");
-            start = start[0]*1 + start[1]/60;
+            start = start[0] * 1 + start[1] / 60;
             var finish = item.Time_finish.split(":");
-            finish = finish[0]*1 + finish[1]/60;
+            finish = finish[0] * 1 + finish[1] / 60;
+
             if (hours >= start && hours >= finish && item.Aud_to) {
-                //console.log(item.Aud_to, group);
-                var coords = _.find(xmlData, function(d) {
+                //console.log(item.Aud_to, roomDict[item.Aud_to], group);
+                coords = _.find(xmlData, function (d) {
                     return d.id == roomDict[item.Aud_to]
                 });
-                if (!coords) {
-
-                    groupLabels[group].style("opacity", 0)
-                } else {
-                    coords = coords.coords;
-                    groupLabels[group].style({
-                        top: yScale(coords.y) + "px",
-                        left: xScale(coords.x) + "px"
-                    })
-                }
-
             }
             if (hours >= start && hours <= finish && item.Aud_from && item.Aud_to) {
                 node1 = roomDict[item.Aud_from];
                 node2 = roomDict[item.Aud_to];
-                //console.log(item.Aud_from, item.Aud_to, node1, node2, group);
-                getData(node1, node2, groupPathes[group]);
+                if (!node1 || !node2) {
+                    console.log(item.Aud_from, item.Aud_to, node1, node2, group);
+                } else {
+                    getData(node1, node2, groupPathes[group]);
+                }
             }
+
         });
+
+        if (!coords) {
+            console.log("!coords", group);
+            groupLabels[group].style("opacity", 0)
+        } else {
+            coords = coords.coords;
+            groupLabels[group].style({
+                opacity: 1,
+                top: yScale(coords.y) + "px",
+                left: xScale(coords.x) + "px"
+            });
+            console.log(coords,group, "moved")
+        }
+
         var global_finish = data[weekDay][group][data[weekDay][group].length - 1].Time_finish.split(":");
-        global_finish = global_finish[0]*1 + global_finish[1]/60;
+        global_finish = global_finish[0] * 1 + global_finish[1] / 60;
         var global_start = data[weekDay][group][0].Time_start.split(":");
-        global_start = global_start[0]*1 + global_start[1]/60;
+        global_start = global_start[0] * 1 + global_start[1] / 60;
         if (hours > global_finish) {
             groupLabels[group]
                 .style({"opacity": "1"})
-                .transition()
-                .duration(500)
+                //.transition()
+                //.duration(500)
                 .style({
                     "opacity": "0.5",
                     top: "130px",
@@ -149,8 +158,8 @@ function updateGroup(data, xmlData) {
         if (hours < global_start && global_start - hours < 0.3) {
             groupLabels[group]
                 .style({"opacity": "0"})
-                .transition()
-                .duration(500)
+                //.transition()
+                //.duration(500)
                 .style({"opacity": "1"})
         }
 
@@ -160,6 +169,7 @@ function updateGroup(data, xmlData) {
 
 
 function updateLabels(points) {
+    console.log(points, "points!");
     var startPoint = points[0];
     var finishPoint = points[points.length - 1];
     startDiv
@@ -175,25 +185,32 @@ function updateLabels(points) {
             top: (yScale(finishPoint.y) - 15) + "px"
         });
 }
+var line = d3.svg.line()
+    .x(function (d) {
+        return xScale(d.x)
+    })
+    .y(function (d) {
+        return yScale(d.y)
+    });
 
 function completePath(path, data) {
     if (!data) return;
-    var line = d3.svg.line()
-        .x(function (d) {
-            return xScale(d.x)
-        })
-        .y(function (d) {
-            return yScale(d.y)
-        });
-
-    path.attr("d", line(data));
-    path.attr("opacity", 0)
+    console.log(data);
+    Object.keys(path).forEach(function(key) {
+        path[key].attr("d", line(data[key]));
+        path[key].attr("opacity", 0)
+            .transition()
+            .duration(1000)
+            .attr("opacity", 1);
+    })
+}
+function completeOnePath(data) {
+    commonPath.attr("d", line(data));
+    commonPath.attr("opacity", 0)
         .transition()
         .duration(1000)
         .attr("opacity", 1);
-
 }
-
 
 var groupLabels = {
     "504_SE": container.select("#group_504_SE"),
@@ -203,20 +220,42 @@ var groupLabels = {
 };
 
 var groupPathes = {
-    "504_SE": svg.append("path").classed("group_504_SE", true),
-    "503_CS": svg.append("path").classed("group_503_CS", true),
-    "504_BI": svg.append("path").classed("group_504_BI", true),
-    "603_CS": svg.append("path").classed("group_603_CS", true)
+    "504_SE": {
+        path2: svg.append("path").classed("group_504_SE floor_2", true),
+        path4: svg.append("path").classed("group_504_SE floor_4", true),
+        path5: svg.append("path").classed("group_504_SE floor_5", true)},
+    "503_CS": {
+        path2: svg.append("path").classed("group_503_CS floor_2", true),
+        path4: svg.append("path").classed("group_503_CS floor_4", true),
+        path5: svg.append("path").classed("group_503_CS floor_5", true)},
+    "504_BI": {
+        path2: svg.append("path").classed("group_504_BI floor_2", true),
+        path4: svg.append("path").classed("group_504_BI floor_4", true),
+        path5: svg.append("path").classed("group_504_BI floor_5", true)},
+    "603_CS": {
+        path2: svg.append("path").classed("group_603_SE floor_2", true),
+        path4: svg.append("path").classed("group_603_SE floor_4", true),
+        path5: svg.append("path").classed("group_603_SE floor_5", true)}
 };
 
 function drawPath(data, custom_path) {
-    data = data.path.map(function (d) {
-        return data[d]
-    });
     if (custom_path) {
+        data = {
+            path2: data.path.map(function (d) {
+                return data[d]
+            }),
+            path4: data.path.map(function (d) {
+                return data[d]
+            }),
+            path5: data.path.map(function (d) {
+                return data[d]
+            })
+        };
         completePath(custom_path, data);
     } else {
-        completePath(commonPath, data);
+        data = data.path.map(function (d) {
+            return data[d]});
+        completeOnePath(data);
         updateLabels(data)
     }
 
@@ -224,29 +263,36 @@ function drawPath(data, custom_path) {
 
 
 function moveTable() {
+    console.log("mavet");
     var groups = Object.keys(groupPathes);
     var data = {};
     var maxLength = 0;
-    groups.forEach(function (d) {
-        var p = groupPathes[d].node();
-        var l = p.getTotalLength();
-        if (l > maxLength) {
-            maxLength = l
-        }
-        data[d] = {
-            path: p,
-            total: l,
-            name: d
-        }
+    groups.forEach(function(d) {
+        var pathes = Object.keys(groupPathes[d]);
+        data[d] = {};
+        pathes.forEach(function(floor_path){
+            var p = groupPathes[d][floor_path].node();
+            var l = p.getTotalLength();
+            if (l > maxLength) {
+                maxLength = l
+            }
+            data[d][floor_path] = {
+                path: p,
+                total: l,
+                name: d
+            }
+        });
+
     });
 
-    d3.transition()
+    /*d3.transition()
         .duration(30 * maxLength)
         .tween("rotate", function () {
             return function (t) {
                 Object.keys(data).forEach(function (key) {
                     var gr = data[key];
                     var coords = gr.path.getPointAtLength(t * gr.total);
+                    console.log(coords);
                     groupLabels[gr.name].style({
                         top: coords.y + "px",
                         left: coords.x + "px"
@@ -258,7 +304,7 @@ function moveTable() {
         .transition()
         .each("end", function () {
             console.log("end")
-        })
+        })*/
 }
 
 function setFloor(floor) {
@@ -278,7 +324,7 @@ function setFloor(floor) {
         d3.select(".button_floor_2").classed("floor-active", true)
     }
     if (floor == 4) {
-        d3.selectAll("img_floor_2, img_floor_5")
+        d3.selectAll("img.floor_2, img.floor_5")
             .style("opacity", 0);
         d3.select("img.floor_4")
             .transition()
