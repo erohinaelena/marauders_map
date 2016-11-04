7dcbd16546e9ecbd180739c9724f9425e738f2c4
bugogish <bugogish@gmail.com>
2016-11-04 11:11:33 +0300

    Path scripts fixed

--- app/views.py
+++ app/views.py
@@ -25,10 +25,9 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
-
 @login_manager.user_loader
 def load_user(id):
-    with open ('app/users/' + id + '.txt', 'r') as user:
+    with open('app/users/' + id + '.txt', 'r') as user:
         for line in user:
             params = line.split()
             return User(params[0], params[1], params[2])
@@ -133,7 +132,7 @@ def index():
 @login_required
 def logout():
     logout_user()
-    return Response('<p>Logged out</p>')
+    return redirect(url_for("login"))
 
 
 # handle login failed
@@ -149,7 +148,8 @@ def check_valid_users():
         print('CURRENT TIME', current_hours, current_minutes)
         print ('USERS LAST LOGIN', users[user]['hours'], users[user]['minutes'])
         tdelta = (
-        int(current_hours) * 60 + int(current_minutes) - int(users[user]['hours']) * 60 - int(users[user]['minutes']))
+            int(current_hours) * 60 + int(current_minutes) - int(users[user]['hours']) * 60 - int(
+                users[user]['minutes']))
         print ('DIFF', tdelta)
         if tdelta >= 90:
             del users[user]
@@ -177,9 +177,9 @@ def get_floor_by_id(node):
         return 5
 
 
-def get_path(G, node1, node2):
+def get_path(node1, node2):
     result = {}
-    path = nx.shortest_path(G, node1, node2)
+    path = nx.shortest_path(G, node1, node2, weight='weight')
     result['path'] = path
 
     path2, path4, path5 = [[x for x in path if get_floor_by_id(x) == i] for i in (2, 4, 5)]
@@ -194,10 +194,30 @@ def get_path(G, node1, node2):
 
 @app.route('/graph/<node1>/<node2>')
 def get_path_json(node1, node2):
-    return json.dumps(get_path(G, node1, node2))
+    return json.dumps(get_path(node1, node2))
 
-# print G.nodes()
 
-# tmp = nx.get_node_attributes(G,'no_room')
+@app.route('/graph/<node1>/wc_m')
+def find_wc_m(node1):
+    wc_m = ['5061', '15', '2123']
+    path = [nx.shortest_path_length(G, node1, x, weight='weight') for x in wc_m]
+    return get_path_json(node1, wc_m[path.index(min(path))])
 
-# print json.dumps(dict((v,k) for k,v in tmp.iteritems()))
+
+@app.route('/graph/<node1>/wc_f')
+def find_wc_f(node1):
+    wc_f = ['5015', '61', '2126']
+    path = [nx.shortest_path_length(G, node1, x, weight='weight') for x in wc_f]
+    return get_path_json(node1, wc_f[path.index(min(path))])
+
+
+@app.route('/graph/<node1>/living_room')
+def find_living_room(node1):
+    liv_room = ['5065', '60', '65']
+    path = [nx.shortest_path_length(G, node1, x, weight='weight') for x in liv_room]
+    return get_path_json(node1, liv_room[path.index(min(path))])
+
+
+@app.route('/graph/<node1>/to_AD')
+def find_academic_department(node1):
+    return redirect(url_for('get_path_json', node1=node1, node2=67))
